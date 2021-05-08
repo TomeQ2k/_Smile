ceec0071101833765ebb9606df77882fbd9a3362
TomeQ2k <48593674+TomeQ2k@users.noreply.github.com>
2021-05-08 21:20:30 +0200

    Update IReportRepository.cs

--- Smile.Core.Domain/Data/Repositories/IReportRepository.cs
+++ Smile.Core.Domain/Data/Repositories/IReportRepository.cs
@@ -8,7 +8,7 @@ namespace Smile.Core.Domain.Data.Repositories
 {
     public interface IReportRepository : IRepository<Report>
     {
-        Task<IPagedList<Report>> GetFilteredUserReports(string currentUserId, IReportFiltersParams filters, (int PageNumber, int PageSize) pagination);
+        Task<IPagedList<Report>> GetFilteredUserReports(string userId, IReportFiltersParams filters, (int PageNumber, int PageSize) pagination);
         Task<IPagedList<Report>> GetFilteredReportsWithReporterName(string reporterName, ReportType reportType, IReportFiltersParams filters, (int PageNumber, int PageSize) pagination);
     }
 }
