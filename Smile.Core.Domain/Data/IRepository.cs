ef057d2ef14cef7e9d1db129e3e558ea7f22470a
tomeq <tomeknowok@gmail.com>
2021-06-29 00:18:06 +0200

    .

--- Smile.Core.Domain/Data/IRepository.cs
+++ Smile.Core.Domain/Data/IRepository.cs
@@ -8,10 +8,10 @@ namespace Smile.Core.Domain.Data
     public interface IRepository<T> where T : class, new()
     {
         Task<T> FindById(string id);
-        Task<T> Find(Expression<Func<T, bool>> predicate, params string[] includes);
+        Task<T> Find(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
 
-        Task<IEnumerable<T>> GetAll(params string[] includes);
-        Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate, params string[] includes);
+        Task<IEnumerable<T>> GetAll(params Expression<Func<T, object>>[] includes);
+        Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
 
         void Add(T entity);
         void AddRange(IEnumerable<T> entities);
